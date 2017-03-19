67ecabd0544c04f365908e950c30ad58e658a3a5
zhoushaotian <245406994@qq.com>
2017-03-19 16:38:32 +0800

    finish

---
+++ index.js
@@ -0,0 +1,78 @@
+var path = require('path');
+var Express = require('express');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var config = require('config-lite');
+var routes = require('./routes/route');
+var pkg = require('./package');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+var app = Express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(Express.static(path.join(__dirname, '/public')));
+
+app.use(session({
+    name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
+    secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+    resave: true, // 强制更新 session
+    saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+    cookie: {
+        maxAge: config.session.maxAge // 过期时间，过期后 cookie 中的 session id 自动删除
+    },
+    store: new MongoStore({ // 将 session 存储到 mongodb
+        url: config.mongodb // mongodb 地址
+    })
+}));
+
+app.use(flash());
+
+app.use(require('express-formidable')({
+    uploadDir: path.join(__dirname, '/public/img'),
+    keepExtensions: true
+}));
+// 设置模板全局常量
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function(req, res, next) {
+    res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.locals.error = req.flash('error').toString();
+    next();
+});
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new(winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'
+        })
+    ]
+}));
+routes(app);
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'
+        })
+    ]
+}));
+
+app.listen(config.port, function() {
+    console.log(`${pkg.name} listening on port ${config.port}`);
+});
---
+++ routes/signout.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+// GET /signout 登出
+router.get('/', checkLogin, function(req, res, next) {
+    req.session.user = null;
+    req.flash('success', '退出成功');
+    res.redirect('/posts');
+});
+
+module.exports = router;
